06b76fa1c79b47125b53a439fa6546bf13c9389f
sarukiti <sarukiti1891@gmail.com>
2023-10-09 16:27:17 +0900

    収穫機構が動く

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use safe_drive::{
+    context::Context, error::DynError, logger::Logger, msg::common_interfaces::std_msgs,pr_info,
+};
+
+fn main() -> Result<(), DynError>{
+    let ctx = Context::new()?;
+    let node = ctx.create_node("demeter", None, Default::default())?;
+    let subscriber = node.create_subscriber::<std_msgs::msg::Int8>("demeter_oracle", None)?;
+    let publisher = node.create_publisher::<drobo_interfaces::msg::MdLibMsg>("md_driver_topic", None)?;
+    let mut pub_msg = drobo_interfaces::msg::MdLibMsg::new().unwrap();
+    pub_msg.address = 0x04;
+    pub_msg.mode = 2;
+    pub_msg.power = 500;
+
+    let logger = Logger::new("demeter");
+
+    let mut selector = ctx.create_selector()?;
+    selector.add_subscriber(
+        subscriber, 
+        Box::new(move |msg| {
+            pub_msg.phase = if msg.data >= 0 {true} else {false};
+            pub_msg.power = if msg.data != 0 {500} else {0};
+            pr_info!(logger, "収穫機構: {}", if msg.data == 1 {"上昇"} else if msg.data == 0 {"ストップ"} else {"下降"});
+            publisher.send(&pub_msg).unwrap();
+        }),
+    );
+
+    loop {
+        selector.wait()?;
+    }
+}
